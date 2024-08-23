433ddec271c64dd222e1fa2f44382193e7f5fd13
Sergey Malinovskiy <malinovskiy@firecode-team.ru>
2024-08-23 16:51:37 +0300

    Refactor archiver for cmd reusable and compact commands

--- cmd/pack.go
+++ cmd/pack.go
@@ -1,12 +1,67 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"awesome-archiver/lib/compression"
+	"awesome-archiver/lib/compression/vlc"
+	"errors"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 var packCmd = &cobra.Command{
 	Use:   "pack",
 	Short: "Pack file",
+	Run:   pack,
+}
+
+const packedExtension = "vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
+func pack(cmd *cobra.Command, args []string) {
+	var encoder compression.Encoder
+
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+
+	method := cmd.Flag("method").Value.String()
+
+	switch method {
+	case "vlc":
+		encoder = vlc.New()
+	default:
+		cmd.PrintErrln("unknown method")
+	}
+
+	filePath := args[0]
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := encoder.Encode(string(data))
+
+	err = os.WriteFile(packedFileName(filePath), packed, 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+func packedFileName(path string) string {
+	fileName := filepath.Base(path)
+
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName)) + "." + packedExtension
 }
 
 func init() {
 	rootCmd.AddCommand(packCmd)
+
+	packCmd.Flags().StringP("method", "m", "", "compression method: vlc")
+	if err := packCmd.MarkFlagRequired("method"); err != nil {
+		panic(err)
+	}
 }
--- cmd/unpack.go
+++ cmd/unpack.go
@@ -1,12 +1,66 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"awesome-archiver/lib/compression"
+	"awesome-archiver/lib/compression/vlc"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 var unpackCmd = &cobra.Command{
 	Use:   "unpack",
 	Short: "Unpack file",
+	Run:   unpack,
+}
+
+// TODO: take extension from file
+const unpackedExtension = "txt"
+
+func unpack(cmd *cobra.Command, args []string) {
+	var decoder compression.Decoder
+
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+
+	method := cmd.Flag("method").Value.String()
+
+	switch method {
+	case "vlc":
+		decoder = vlc.New()
+	default:
+		cmd.PrintErrln("unknown method")
+	}
+
+	filePath := args[0]
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := decoder.Decode(data)
+
+	err = os.WriteFile(unpackedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+// TODO: refactor this
+func unpackedFileName(path string) string {
+	fileName := filepath.Base(path)
+
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName)) + "." + unpackedExtension
 }
 
 func init() {
 	rootCmd.AddCommand(unpackCmd)
+
+	unpackCmd.Flags().StringP("method", "m", "", "de-compression method: vlc")
+	if err := unpackCmd.MarkFlagRequired("method"); err != nil {
+		panic(err)
+	}
 }
